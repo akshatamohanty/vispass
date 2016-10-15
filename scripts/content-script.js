d90973eb4d73455b750e17b74ea0e01a98b9b03a
akshatamohanty <akshatamohanty@gmail.com>
2016-10-15 19:18:42 +0800

    content script modififed

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,16 +1,33 @@
-var $btn = $(':password').closest('form').find(':submit');
-$btn.prop('disabled', true);
+$(document).ready(function(){
 
-var message = $btn.val();
-$btn.val("Locked By VisPass");
+	var password = $(':password');
+	var submit_btn = password.closest('form').find(':submit');
+
+	var str = password.val(); setTimeout( function(){
+											console.log(str);
+											console.log(password.get(0).value)
+										}, 1000);
+	var regex = /[^\w\s]/gi;
+
+	alert(password.get(0).value);
+
+	if(regex.test(str) == true) {
+	    alert('Your search string contains illegal characters.');
+	    submit_btn.prop('disabled', true);
+	}
+
+	var message = submit_btn.val();
+	//submit_btn.val("Locked By VisPass");
 
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+	chrome.runtime.onMessage.addListener(
+	    function(request, sender, sendResponse) {
 
-		$btn.prop('disabled', false);
-		$btn.val(message);
+			submit_btn.prop('disabled', false);
+			submit_btn.val(message);
 
-        sendResponse({farewell: "goodbye"})
-	});
+	        sendResponse({farewell: "goodbye"})
+		});
+
+});
 
